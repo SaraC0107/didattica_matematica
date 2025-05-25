90d55097596468cf9ee7015c3e09d9f5d4e1bb44
saraC0107 <saracastell2000@gmail.com>
2025-05-25 18:16:20 +0200

    changed readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Gioco di musica per il progetto di didattica della matematica.
 Suddivisione a step per attività bambini.
+Per runnare gli step, usare il comando "pgzrun Step_1.py"
--- Step_1.py
+++ Step_1.py
@@ -9,8 +9,8 @@ import time
 # ============================================================================================
 # STEP 1: DEFINITE LE DIMENSIONI DELLA FINESTRA DI GIOCO (WIDTH = LARGHEZZA, HEIGHT = ALTEZZA)
 # ============================================================================================
-WIDTH = 
-HEIGHT = 
+WIDTH =
+HEIGHT =
 
 background = pygame.image.load("images/background.jpg")
 background = pygame.transform.smoothscale(background, (WIDTH, HEIGHT))
